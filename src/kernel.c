9c46fd69892aed18be6b14cb0499566a58dbc2d3
Pietro Paolini <competitiveprogramming.pietro@gmail.com>
2025-09-20 22:08:44 +0100

    Kernel screen hello world in C

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,54 @@
 #include "kernel.h"
+#include <stdint.h>
+
+
+#define COLOR_TERMINAL(c,col) ((col << 8) | c)
+
+uint16_t * video_memory = 0;
+uint16_t current_col, current_row;
+
+// Simple function to write a string on the screen
+void write_string(char * str) {
+	
+	while (*str) {
+
+		if (*str == '\n') {
+			current_col = 0;
+			current_row = (current_row + 1) % VGA_ROWS;
+			str++;
+			continue;
+		}
+
+
+		video_memory[current_row * VGA_COLS + current_col] = COLOR_TERMINAL(*str, 0x2);
+		
+		if ( (current_col + 1) % VGA_COLS == 0) {
+			current_col = 0;
+			current_row = (current_row + 1) % VGA_ROWS;
+			str++;
+			continue;
+		}
+		current_col++;
+		str++;
+	}
+}
 
 void kernel_main() {
+  
+  video_memory = (uint16_t*) 0xB8000;
 
+  /* Blank out the screen */
+  for (int i = 0; i < VGA_ROWS; i++) {
+  	for (int j = 0; j < VGA_COLS; j++) {
+		video_memory[i * VGA_COLS + j] = COLOR_TERMINAL(' ', 0x0);
+	}
+  }
+
+  current_col = current_row = 0;
+  // base_framebuffer[0] = 0x0341; // Endianess makes it 0x41 0x3 - letter 'A' color green
+
+  write_string("Hello world\nThis is a BRAND NEW OS!");
 }
+
+
+
--- src/kernel.h
+++ src/kernel.h
@@ -1,5 +1,10 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#define VGA_COLS 80
+#define VGA_ROWS 20
+
+#define COLOR_TERMINAL(c,col) ((col << 8) | c)
+
 void kernel_main();
 #endif
